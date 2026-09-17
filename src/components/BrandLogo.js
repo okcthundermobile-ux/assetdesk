2908e424fa320a685fa6f38a1ca939fc64520bc8
gavendano22 <gavendano22@ou.edu>
2026-09-17 13:53:25 -0500

    Point the app logo at the newly uploaded PNG in public/logo.png

--- src/components/BrandLogo.js
+++ src/components/BrandLogo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BrandLogo({ size = 34 }) {
   return (
     <img
-      src="/innovations-hdr-25.avif"
+      src="/logo.png"
       alt="Oklahoma City Thunder"
       width={size}
       height={size}
